772820f585d8d400a33a4f5c812d5b0a6427e461
Aziz Jalel <azizjalelcontact@gmail.com>
2023-01-07 09:58:25 -0600

    Added navbar, import react bootstrap, and changed App.jsx file

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+// Create a header component to show the logo and the title of the app.
+//Using React bootstarap
+// Add the following code to the src/components/Header.jsx file:
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown'; // import logo from '../logo.svg';
+
+
+export default function NavbarApp() {
+  return (
+    <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg">
+      <Container >
+        <Navbar.Brand href="#">NUProjects</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">FAQ</Nav.Link>
+            {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown> */}
+            {/* <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link> */}
+          </Nav>
+
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          {/* Add Profile */}
+          <Nav>
+          <NavDropdown title="Profile" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Settings</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Jobs
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Sign out
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
