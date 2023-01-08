5ad32618e39a50b049a7ca0af35689822ae2e5c0
TimHuynh0905 <timhuynh0905@gmail.com>
2023-01-08 16:50:55 -0600

    designed viewmodal

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import NavbarApp from './components/Navbar';
-import CardApp from './components/Card';
-import ViewModal from './components/ViewModal';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import NavbarApp from "./components/Navbar";
+import CardApp from "./components/Card";
+import ViewModal from "./components/ViewModal/ViewModal";
 
 //
 
 const App = () => {
-
   return (
     <div className="App">
       <NavbarApp />
-      <CardApp projectName="NU Dance Marathon" positionName="Front-end Developer" description="Description here..." datePosted="2 days ago"
-               tags={["#frontend", "#dancemarathon"]} />
+      <CardApp
+        projectName="NU Dance Marathon"
+        positionName="Front-end Developer"
+        description="Description here..."
+        datePosted="2 days ago"
+        tags={["#frontend", "#dancemarathon"]}
+      />
     </div>
   );
 };
---
+++ src/components/ViewModal/ViewModal.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import "./ViewModal.css";
+import { Row, Col } from "react-bootstrap";
+
+const ViewModal = ({ applicationData, show, toggleShow }) => {
+  // applicationData = {
+  //   "projectName": "NAME",
+  //   "typeOfProject": "Personal Project",
+  //   "hashtags": ["ML", "AI", "Web Dev"],
+  //   "positionName": "FrontEnd Developer",
+  //   "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
+  //   "wage": 10,
+  //   "deadline": "DEADLINE",
+  //   "datePosted": "POSTED DATE",
+  //   "user": "USER",
+  //   "contactInfo": "sengdao@boss.com",
+  //   "timeline": "TIMELINE",
+  //   "numberOfPeople": 5,
+  //   "skillsRequired": ["Python", "Flask", "HTML"]
+  // }
+
+  //   const [show, setShow] = useState(false)
+  //   const handleClose = () => setShow(false)
+  //   const handleShow = () => setShow(true)
+
+  return (
+    <Modal show={show} onHide={toggleShow} className="modal">
+      <Modal.Header className="modal_header" closeButton>
+        <Modal.Title>{applicationData.projectName}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="modal_body">
+        <h5>Details</h5>
+        <Row>
+          <Col lg={6} sm={12}>
+            <b>Position:</b> {applicationData.positionName}
+          </Col>
+          <Col lg={6} sm={12}>
+            <b>Wage:</b> {applicationData.wage}
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={6} sm={12}>
+            <b>Type:</b> {applicationData.typeOfProject}
+          </Col>
+          <Col lg={6} sm={12}>
+            <b>Timeline:</b> {applicationData.timeline}
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={6} sm={12}>
+            <b>No. of People:</b> {applicationData.numberOfPeople}
+          </Col>
+          <Col lg={6} sm={12}>
+            <b>Dated Posted:</b> {applicationData.datePosted}
+          </Col>
+        </Row>
+        <hr />
+        <h5>Description</h5>
+        <p>{applicationData.description}</p>
+      </Modal.Body>
+      <Modal.Footer className="justify-content-space-between">
+        <p>
+          <b>Contact Info:</b> {applicationData.contactInfo}
+        </p>
+        <Button
+          style={{
+            backgroundColor: "blueviolet",
+            border: "none",
+          }}
+          onClick={toggleShow}
+        >
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+/*
+
+project name, type of project, position name, description, wage, deadline, posted date, 
+contactinfo, job timeline, number of people, skills required
+
+*/
+
+export default ViewModal;
