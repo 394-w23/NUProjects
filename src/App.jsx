9f637e69a8e0adcbd9fdab767e9693258c346bb3
patrickhoey20 <patrickhoey2024@u.northwestern.edu>
2023-01-08 14:10:58 -0600

    attempting to connect backend to frontend (cards)

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavbarApp from './components/Navbar';
 import CardApp from './components/Card';
-
-//
+import { useDbData } from './utilities/firebase';
+import TestDB from './components/TestDB';
 
 const App = () => {
+  const data = useDbData();
+  // const cards = Object.values(data[0]['jobs']).map(card => {
+  //   return (
+  //     <CardApp projectName={card.projectName} positionName={card.projectName} description={card.projectName} datePosted={card.projectName}
+  //     tags={card.projectName}/>
+  //   )
+  // });
   return (
     <div className="App">
       <NavbarApp />
-      <CardApp projectName="NU Dance Marathon" positionName="Front-end Developer" description="Description here..." datePosted="2 days ago"
-               tags={["#frontend", "#dancemarathon"]} />
+      {/* {cards} */}
+      <TestDB />
     </div>
   );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,20 +6,20 @@ import "./Card.css";
 export default function CardApp(props) {
     return (
         <Card className="job-card">
-            <Card.Header>{props.applicationData.projectName}</Card.Header>
+            <Card.Header>{props.projectName}</Card.Header>
             <Card.Body>
-            <Card.Title>{props.applicationData.positionName}</Card.Title>
+            <Card.Title>{props.positionName}</Card.Title>
             <Card.Text>
-                {props.applicationData.description}
+                {props.description}
             </Card.Text>
             <ul className="hashtags">
-                {props.applicationData.tags.map((tag, i) => (
+                {(props.tags).map((tag, i) => (
                     <li key={i}>{tag}</li>
                 ))}
             </ul>
             <Button variant="primary">See more</Button>
             </Card.Body>
-            <Card.Footer className="text-muted">{props.applicationData.datePosted}</Card.Footer>
+            <Card.Footer className="text-muted">{props.datePosted}</Card.Footer>
         </Card>
     );
 }
