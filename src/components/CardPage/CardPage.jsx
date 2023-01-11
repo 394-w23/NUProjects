6215e14822cbf5a201a49bc93f55614f7fcae50c
salomekhel <78006235+salomekhel@users.noreply.github.com>
2023-01-10 20:23:19 -0600

    Update CardPage.jsx

--- src/components/CardPage/CardPage.jsx
+++ src/components/CardPage/CardPage.jsx
@@ -1,17 +1,36 @@
 import CardApp from "./Card";
 import Database from "../Database";
+import { useDbData } from '../../utilities/firebase';
 
 export default function CardPageApp() {
-  const data = Database();
+  const [data, error] = useDbData();
+  console.log(data);
   let jobs = null;
   let users = null;
   let cards = null;
+  
+
   if (data) {
-    jobs = data[0];
-    users = data[1];
+    jobs = data.jobs;
+    console.log(jobs);
+    users = data.users;
+    const testJobs = [jobs];
+    testJobs.sort((a, b) => {
+      if ( b.dataPosted == null) {
+        if (a.dataPosted == null){
+          return false
+        }
+        return false
+      }
+      return new Date(a.datePosted) - new Date(b.datePosted); // descending order
+    })
+    console.log("This is testJobs")
+    console.log(testJobs)
+
     cards = Object.values(jobs).map((card, i) => {
       return <CardApp key={i} data={card} />;
     });
   }
   return <>{cards}</>;
 }
+
