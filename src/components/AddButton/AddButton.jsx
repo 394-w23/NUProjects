454190d0fe26437ce9072f42dab8ce4105eb68ea
Aziz Jalel <azizjalelcontact@gmail.com>
2023-01-20 10:47:28 -0600

    Aziz & Lucy: Adding auth authentification w/ google

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import AddModal from '../AddModal/AddModal';
 import "./AddButton.css";
+import { useAuthState } from '../../utilities/firebase';
 
 export default function AddButtonApp() {
     const [showAddModal, setShowAddModal] = useState(false);
     const toggleShowAddModal = () => setShowAddModal(!showAddModal);
 
+    
     return (
         <>
           <Button className="add-button" onClick={toggleShowAddModal}>Add New Position Listing</Button>
