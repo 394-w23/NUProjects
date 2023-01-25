a39745e058bce5b37f099f776b220deca979b993
Lucy Beck <lucywbeck@Lucys-Air-2.attlocal.net>
2023-01-25 11:32:27 -0600

    make styling uniform

--- src/components/CardPage/Card.jsx
+++ src/components/CardPage/Card.jsx
@@ -7,26 +7,39 @@ export default function CardApp({ data }) {
   const [showViewModal, setShowViewModal] = useState(false);
   const toggleShowViewModal = () => setShowViewModal(!showViewModal);
 
-  const hashtags = Object.values(data.hashtags).map((hashtag, i) => {
-    return <li key={i}>{hashtag}</li>;
-  });
-
   return (
     <>
       <Card className="job-card">
         <Card.Header>
-          <b>{data.projectName}</b> | {data.numberOfPeople} members
+          <b>{data.projectName}</b> | {data.numberOfPeople} member(s)
         </Card.Header>
         <Card.Body>
           <Card.Title>{data.positionName}</Card.Title>
           <Card.Text>{data.description}</Card.Text>
-          <ul className="hashtags">{hashtags}</ul>
+          <ul className="skills">
+            {data.skillsRequired.map((skill, id) => {
+              return (
+                <li className="skill" key={id}>
+                  {skill}
+                </li>
+              );
+            })}
+          </ul>
+          <ul className="skills">
+            {data.hashtags.map((hashtag, id) => {
+              return (
+                <li className="skill" key={id}>
+                  {hashtag}
+                </li>
+              );
+            })}
+          </ul>
           <Button onClick={toggleShowViewModal} variant="primary">
             See more
           </Button>
         </Card.Body>
         <Card.Footer className="text-muted">
-          Date posted: {data.datePosted} | Deadline: {data.dateToSubmit}
+          Date posted: {data.datePosted} | Application Deadline: {data.dateToSubmit}
         </Card.Footer>
       </Card>
       <ViewModal
