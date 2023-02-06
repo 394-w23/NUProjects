748413b34f630786e4e95d6f88161a74d3e8c8cd
Lucy Beck <lucywbeck@Lucys-Air-2.attlocal.net>
2023-02-06 14:09:29 -0600

    Test HomePage When Signed Out

--- cypress/e2e/HomePage.cy.js
+++ cypress/e2e/HomePage.cy.js
@@ -1,6 +1,6 @@
 /* globals cy */
     
-describe ('Test HomePage', () => {
+describe ('Test HomePage When Signed In', () => {
 
     beforeEach(() => {
       cy.visit("/");
@@ -32,6 +32,46 @@ describe ('Test HomePage', () => {
 
     it("opens with add new posiiton listing button", () => {
       cy.get("[data-cy=add-button]").should("contain", "Add New Position Listing");
+    });
+    
+  });
+
+  describe ('Test HomePage When Signed Out', () => {
+
+    beforeEach(() => {
+      cy.visit("/");
+      cy.get("[id=collapsible-nav-dropdown]").click();
+      cy.get("[data-cy=sign-out]").click();
+    });
+
+    it ('opens with page title', () => {
+      cy.get('[data-cy=pageTitle]').should('contain', 'NUProjects');
+    });
+
+    it ('opens with home nav', () => {
+      cy.get("[data-cy=home]").should("contain", "Home");
+    });
+
+    it("opens with sign in button and does not contain collapsible nav dropdown items", () => {
+      cy.get("[id=collapsible-nav-dropdown]").should('not.exist');
+      cy.get("[data-cy=profile]").should('not.exist');
+      cy.get("[data-cy=saved]").should('not.exist');
+      cy.get("[data-cy=applied]").should('not.exist');
+      cy.get("[data-cy=sign-out]").should('not.exist');
+      cy.get("[data-cy=sign-in]").should("contain", "Sign in");
+    });
+
+    it("opens with search, sort, and filter", () => {
+      cy.get('input[type="search"]').should("have.value", "")
+
+      cy.get("[data-cy=sorting-button]").invoke("val").should("eq", "datePosted");
+
+      cy.get("[data-cy=filtering-button]").invoke("val").should("eq", "");
+    });
+
+    it("opens with add new posiiton listing button disabled", () => {
+      cy.get("[data-cy=add-button]").should("contain", "Add New Position Listing");
+      cy.get("[data-cy=add-button]").should('be.disabled')
     });
     
   });
