c229688c7f245af024cb4c68fcd0135b25e1697f
Aiden <azizbenabderrahmen2024@u.northwestern.edu>
2023-03-08 16:51:03 -0600

    Create App.test.jsx

---
+++ src/App.test.jsx
@@ -0,0 +1,14 @@
+import {describe, expect, test} from 'vitest';
+import {fireEvent, render, screen} from '@testing-library/react';
+import App from './App';
+
+describe('unit tests', () => {
+
+  test("Website should load", () => {
+    render(<App />);
+    expect(screen.getByText('Sign in')).toBeDefined();
+    expect(screen.getByText('NUProjects')).toBeDefined();
+  });
+
+
+});
