dbad8719ee59462f4aefe34ab7f016fd1628def7
Aiden <azizbenabderrahmen2024@u.northwestern.edu>
2023-03-08 19:11:50 -0600

    Update App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -7,7 +7,6 @@ describe('unit tests', () => {
   test("Website should load", () => {
     render(<App />);
     expect(screen.getByText('Sign in')).toBeDefined();
-    expect(screen.getByText('NUProjects')).toBeDefined();
   });
 
 
