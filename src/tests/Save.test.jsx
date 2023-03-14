176e949fb32750bf30293194e4c89a31b441ff8f
salomekhel <78006235+salomekhel@users.noreply.github.com>
2023-03-14 18:57:40 -0500

    Update Save.test.jsx

--- src/tests/Save.test.jsx
+++ src/tests/Save.test.jsx
@@ -1,1 +1,32 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
 
+describe('Saving a job', () => {
+  it('should add the job to the "Jobs Saved" page', async () => {
+    // Render the app
+    render(<App />);
+
+    // Wait for the job listings to load
+    await act(async () => {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    });
+
+    // Click on the first job listing
+    const jobListings = screen.getAllByTestId('job-listing');
+    fireEvent.click(jobListings[0]);
+
+    // Click the "Save Job" button
+    const saveJobBtn = screen.getByTestId('save-job-btn');
+    fireEvent.click(saveJobBtn);
+
+    // Navigate to the "Jobs Saved" page
+    const jobsSavedLink = screen.getByText('Jobs Saved');
+    fireEvent.click(jobsSavedLink);
+
+    // Assert that the saved job is on the page
+    const savedJobListings = screen.getAllByTestId('saved-job-listing');
+    expect(savedJobListings.length).toBe(1);
+    expect(savedJobListings[0]).toHaveTextContent('Job Title');
+  });
+});
