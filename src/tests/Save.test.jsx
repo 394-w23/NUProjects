96e2bb5b01734591790a068958d4f9ed40abb882
salomekhel <78006235+salomekhel@users.noreply.github.com>
2023-03-15 00:16:26 -0500

    Update Save.test.jsx

--- src/tests/Save.test.jsx
+++ src/tests/Save.test.jsx
@@ -4,6 +4,10 @@ import App from '../App';
 
 describe('Saving a job', () => {
   it('should add the job to the "Jobs Saved" page', async () => {
+    // Mock the fetch API
+    const mockResponse = { status: 200, body: { id: 123, title: 'Job Title' } };
+    global.fetch = jest.fn(() => Promise.resolve(mockResponse));
+
     // Render the app
     render(<App />);
 
@@ -20,13 +24,20 @@ describe('Saving a job', () => {
     const saveJobBtn = screen.getByTestId('save-job-btn');
     fireEvent.click(saveJobBtn);
 
-    // Navigate to the "Jobs Saved" page
-    const jobsSavedLink = screen.getByText('Jobs Saved');
-    fireEvent.click(jobsSavedLink);
+    // Assert that the fetch API was called with the correct arguments
+    expect(global.fetch).toHaveBeenCalledWith('/api/save-job', {
+      method: 'POST',
+      body: JSON.stringify({ id: 123, title: 'Job Title' }),
+      headers: { 'Content-Type': 'application/json' }
+    });
 
-    // Assert that the saved job is on the page
-    const savedJobListings = screen.getAllByTestId('saved-job-listing');
-    expect(savedJobListings.length).toBe(1);
-    expect(savedJobListings[0]).toHaveTextContent('Job Title');
+    // Assert that the saved job is on the "Jobs Saved" page
+    const savedJobs = await screen.findAllByTestId('saved-job');
+    expect(savedJobs.length).toBe(1);
+    expect(savedJobs[0]).toHaveTextContent('Job Title');
+
+    // Clean up
+    delete global.fetch;
   });
 });
+
